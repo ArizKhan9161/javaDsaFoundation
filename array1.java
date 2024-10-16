ae6480b0396d07f9127adcec4db2f7e53366b276
Ariz Khan <152072779+ArizKhan9161@users.noreply.github.com>
2024-10-16 22:02:50 +0530

    Add files via upload
    problem 1 => two number sum is equal to the target sum
    2 =>three number sum is equal to the target
    3=>detect repetitive element
    4=> find the second largest element

---
+++ array1.java
@@ -0,0 +1,57 @@
+package Array;
+
+public class array1 {
+    public static void main(String[] args) {
+//        int arr [] ={ 4 , 6 ,3, 5, 8,2 };// two number sum
+//        int target=7 ;
+//        for (int i = 0 ; i<arr.length ; i++){
+//            for(int j =i ; j< arr.length  ;j++){
+//                if(arr[i]+arr[j]==target){
+//                    System.out.println(arr[i] +" ,"+arr[j]);
+//                }
+//            }
+//
+//        }
+        // three number sum
+      //  int arr []= { 1 ,2 , 3 , 4 , 0 , 6 , 4 , 1 , 8};
+        // logic three loop which is very bad
+        // optimization approach is on way
+
+
+        // array manipulation find the unique number in a given array where all the element are being repeated
+        // logic in brute force using two loop and find unique element
+        // int arr [] = {1 , 2 , 3 , 4 , 3 , 2 , 1};
+        // optimization code is coming..
+
+
+
+
+
+//        // find the second largest element
+//        int arr[]={1,2,3,4,5,7,8};
+//        int max=Integer.MIN_VALUE;
+//        for(int i=0 ; i<arr.length ; i++){
+//         if(arr[i]>max){
+//             max=arr[i];
+//         }
+//           }
+//        for (int i =0;i<arr.length;i++){
+//            if(arr[i]==max){
+//                arr[i]=Integer.MIN_VALUE;
+//            }
+//        }
+//        max=Integer.MIN_VALUE;
+//        for(int i =0 ; i<arr.length;i++){
+//            if(arr[i]>max){
+//                max=arr[i];
+//            }
+//        }
+//        System.out.println(max);
+        //optimization code is here
+
+        // first repeating value if no value repeat return -1
+        //logic use two loop and checked one by one  brute force approach
+        // optimization approach
+        
+    }
+}
